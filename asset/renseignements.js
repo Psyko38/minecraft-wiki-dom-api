bb3d1a056503126e94c67a853ec5f340a5a668a6
Psyko38 <110417117+Psyko38@users.noreply.github.com>
2025-04-18 14:09:58 +0200

    add

---
+++ asset/renseignements.js
@@ -0,0 +1,11 @@
+const NavBTN = document.querySelector(".burger");
+const MediaBTN = document.querySelector(".MediaBTN");
+const MediaUi = document.querySelector(".media");
+
+NavBTN.addEventListener("click", () => {
+	MediaUi.style.display = "unset";
+});
+
+MediaBTN.addEventListener("click", () => {
+	MediaUi.style.display = "none";
+});
